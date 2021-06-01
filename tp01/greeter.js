dd321663ab4299a8f8977854899282606ad20cd0
luckydu43 <luc.duperron@laposte.net>
2021-06-01 12:10:00 +0200

    maj notes, maj script TypeScript, ajout de la structure du projet Angular Tour Of Heroes

--- tp01/greeter.js
+++ tp01/greeter.js
@@ -4,3 +4,11 @@ function greeter(person) {
 var user = "luckydu43";
 var hello = greeter(user);
 console.log(hello);
+function greeterInterface(person) {
+    return "Hello "
+        + person.nom
+        + " ayant un skill de type "
+        + person.skill;
+}
+var userInterfaced = { nom: "Luckydu43", skill: "Noob" };
+console.log(greeterInterface(userInterfaced));
--- tp01/greeter.ts
+++ tp01/greeter.ts
@@ -1,4 +1,4 @@
-function greeter(person) {
+function greeter(person: string) {
     return "Hello " + person;
 }
 
@@ -6,3 +6,22 @@ let user = "luckydu43";
 let hello = greeter(user);
 
 console.log(hello)
+
+//let hellonumber = greeter(42);
+//console.log(hellonumber)
+
+interface Humain {
+    nom: string;
+    skill: string;
+  }
+  
+function greeterInterface (person:Humain) {
+    return "Hello " 
+    + person.nom 
+    + " ayant un skill de type " 
+    + person.skill;
+}
+  
+var userInterfaced:Humain = { nom: "Luckydu43", skill: "Noob" };
+  
+console.log(greeterInterface(userInterfaced));
