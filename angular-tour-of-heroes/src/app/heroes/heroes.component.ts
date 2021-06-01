47d9a0f0f388f7b166baba061f7cce95a686053c
luckydu43 <luc.duperron@laposte.net>
2021-06-01 15:27:01 +0200

    maj angular

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  
+  hero1: Hero = {
+    id: 1,
+    nom: 'Windstorm',
+    skill: 'moyen mais a du potentiel'
+  };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onKeyup(e:any) {
+    this.hero1.nom = e.target.value
   }
 
 }
---
+++ angular-tour-of-heroes/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from '../hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, nom: 'Dr Nice', skill: 'au niveau de Brice'},
+  { id: 12, nom: 'Narco', skill: 'suffisant pour diriger un cartel'},
+  { id: 13, nom: 'Bombasto', skill: ' = pète un peu tout'},
+  { id: 14, nom: 'Celeritas', skill: ' = va vite'},
+  { id: 15, nom: 'Magneta', skill: ' = domine Magneto'},
+  { id: 16, nom: 'RubberMan', skill: ' = n\'a jamais réussi à s\'arracher une dent'},
+  { id: 17, nom: 'Dynama', skill: ' = peut faire briller une ampoule avec une dynamo dans sa poche'},
+  { id: 18, nom: 'Dr IQ', skill: ' = extraordinairement con'},
+  { id: 19, nom: 'Magma', skill: ' = néant'},
+  { id: 20, nom: 'Tornado', skill: ' = fait bouger très vite sa main'}
+];
